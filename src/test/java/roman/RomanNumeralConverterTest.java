8a863581cc41b7ed7dee8091281086617f7e61eb
Michael Fan <michaelyfan100@gmail.com>
2026-05-19 23:22:13 -0700

    Add test

--- src/test/java/roman/RomanNumeralConverterTest.java
+++ src/test/java/roman/RomanNumeralConverterTest.java
@@ -1,5 +1,12 @@
 package roman;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RomanNumeralConverterTest {
 
     private RomanNumeralConverter converter;
@@ -11,6 +18,22 @@ public class RomanNumeralConverterTest {
 
     @Test
     void testBasic() {
-        assertEquals("IV", );
+        assertEquals("IV", converter.toRoman(4));
+    }
+
+    @ParameterizedTest(name = "{1} expected for {0}")
+    @CsvSource({
+            "IV, 4",
+            "IX, 9",
+            "XIII, 13",
+            "XL, 40",
+            "XC, 90",
+            "CD, 400",
+            "CMI, 901",
+            "MCMXCIV, 1994",
+            "MMMDCCCLXXXVIII, 3888"
+    })
+    void testMultiple(String expected, int input) {
+        assertEquals(expected, converter.toRoman(input));
     }
 }
